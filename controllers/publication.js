da4eb93f4fe17f0fdc0cada260ff0e161542998f
Junior <juniorjavierduquevalera@gmail.com>
2023-09-25 13:01:38 -0400

    publication delete

--- controllers/publication.js
+++ controllers/publication.js
@@ -42,8 +42,84 @@ const save = async (req, res) => {
   }
 };
 
-module.exports = {
-  save,
+//sacar una publicacion//
+const detail = async (req, res) => {
+  try {
+    // Sacar el ID de la publicación de la URL
+    const publicationId = req.params.id;
+
+    // Buscar la publicación en la base de datos por su ID
+    const publication = await Publication.findById(publicationId);
+
+    // Verificar si la publicación se encontró
+    if (!publication) {
+      return res.status(404).json({
+        status: "error",
+        message: "Publicación no encontrada",
+      });
+    }
+
+    // Devolver una respuesta exitosa con los detalles de la publicación
+    return res.status(200).json({
+      status: "success",
+      message: "Mostrar publicación",
+      publication,
+    });
+  } catch (error) {
+    // Manejar errores y enviar una respuesta de error
+    return res.status(500).json({
+      status: "error",
+      message: "Error al mostrar la publicación",
+      error: error.message,
+    });
+  }
 };
 
+const deletePost = async (req, res) => {
+  try {
+    // Sacar el ID de la publicación a eliminar de la URL
+    const publicationId = req.params.id;
 
+    // Buscar la publicación en la base de datos por su ID y el usuario autenticado
+    const publication = await Publication.findOne({
+      _id: publicationId,
+      user: req.user.id,
+    });
+
+    // Verificar si la publicación se encontró
+    if (!publication) {
+      return res.status(404).json({
+        status: "error",
+        message: "Publicación no encontrada o no tienes permiso para eliminarla",
+      });
+    }
+
+    // Obtener los datos de la publicación antes de eliminarla
+    const deletedPublication = { ...publication._doc };
+
+    // Eliminar la publicación de la base de datos utilizando deleteOne
+    await publication.deleteOne();
+
+    // Devolver una respuesta exitosa junto con los datos de la publicación eliminada
+    return res.status(200).json({
+      publicationId,
+      status: "success",
+      message: "Publicación eliminada correctamente",
+      deletedPublication,
+      
+    });
+  } catch (error) {
+    // Manejar errores y enviar una respuesta de error
+    return res.status(500).json({
+      status: "error",
+      message: "Error al eliminar la publicación",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  save,
+  detail,
+  deletePost,
+};
--- routes/publication.js
+++ routes/publication.js
@@ -5,5 +5,8 @@ const check = require("../middlewares/auth");
 
 //definir rutas//
 router.post("/save", check.auth, publicationController.save);
+router.get("/detail/:id", check.auth, publicationController.detail);
+router.delete("/deletepost/:id", check.auth, publicationController.deletePost);
+
 
 module.exports = router;
